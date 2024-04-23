ecb0d3f637b941bfde3c87aafe5ed63db9a72bf5
gustso <gustso@gustsoNT>
2024-04-23 16:36:46 -0300

    ejercicio02 resuelto

--- src/ar/edu/unju/fi/ejercicio02/main/Main.java
+++ src/ar/edu/unju/fi/ejercicio02/main/Main.java
@@ -1,10 +1,19 @@
 package ar.edu.unju.fi.ejercicio02.main;
 
+import ar.edu.unju.fi.ejercicio02.constants.Mes;
+import ar.edu.unju.fi.ejercicio02.model.Efemerides;
+import ar.edu.unju.fi.ejercicio02.util.Listado;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Efemerides efemerides = new Efemerides("e01",Mes.ABRIL,5,"dia de la marmota");
+		
+		Listado.listadoEfemerides.add(efemerides);
+		
+		System.out.println("Lista: "+Listado.listadoEfemerides.get(0));
 	}
 
 }
--- src/ar/edu/unju/fi/ejercicio02/model/Efemerides.java
+++ src/ar/edu/unju/fi/ejercicio02/model/Efemerides.java
@@ -1,8 +1,59 @@
 package ar.edu.unju.fi.ejercicio02.model;
 
+import ar.edu.unju.fi.ejercicio02.constants.Mes;
+
 public class Efemerides {
 	private String codigo;
-	private int mes;
+	private Mes mes;
 	private int dia;
+	private String detalle;
 
+	public Efemerides() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Efemerides(String codigo, Mes mes, int dia, String detalle) {
+		this.codigo = codigo;
+		this.mes = mes;
+		this.dia = dia;
+		this.detalle = detalle;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public Mes getMes() {
+		return mes;
+	}
+
+	public void setMes(Mes mes) {
+		this.mes = mes;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public String getDetalle() {
+		return detalle;
+	}
+
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+
+	@Override
+	public String toString() {
+		return "Efemerides [codigo=" + codigo + ", mes=" + mes + ", dia=" + dia + ", detalle=" + detalle + "]";
+	}	
+	
 }
